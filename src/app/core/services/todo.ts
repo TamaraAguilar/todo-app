21023ce7b4337a00d144752b8eee3def1caa4723
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-08-09 18:23:23 -0400

    feat: add todo service

---
+++ src/app/core/services/todo.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../../shared/models/todo.model';
+
+@Service()
+export class TodoService {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.apiUrl}?_limit=10`);
+  }
+}
